a2d464ccce7edcc813211b66b05d88760cbfb03a
Priyan Perera <priyan.perera@gmail.com>
2019-03-15 14:26:18 +0530

    Add register component.

--- client/src/app/_services/user.service.ts
+++ client/src/app/_services/user.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { User } from '@/_models';
+import { User } from '../_models/index';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${config.apiUrl}/users`);
+        return this.http.get<User[]>(config.apiUrl + '/users');
+    }
+
+    getById(id: string) {
+        return this.http.get(config.apiUrl + '/users/' + id);
+    }
+
+    create(user: User) {
+        return this.http.post(config.apiUrl + '/users/create', user);
+    }
+
+    update(user: User) {
+        return this.http.put(config.apiUrl + '/users/' + user.id, user);
+    }
+
+    delete(id: string) {
+        return this.http.delete(config.apiUrl + '/users/' + id);
     }
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService } from '../_services/index';
+
+@Component({
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
+    submitted = false;
+    loading = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            firstname: ['', Validators.required],
+            lastname: ['', Validators.required]
+        });
+    }
+
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.userService.create(this.registerForm.value)
+        .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
